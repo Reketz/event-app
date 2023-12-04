0ca4df4cf464279d2eeeac2138d8be796582cd0b
Guilherme Estevao <guilherme.estevao@sankhya.com.br>
2023-12-04 10:56:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+function handleClick() {
+    console.log('clicou no h1');
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        document.getElementById('evento')?.addEventListener('click', handleClick);
+    }, []);
+
+    return (
+        <div className="App">
+            <h1 id='evento'>Clique</h1>
+        </div>
+    );
 }
 
 export default App;
